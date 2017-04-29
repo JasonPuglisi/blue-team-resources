d5ae4c02007ff9f900e898109051c9041eb76885
Jason Puglisi <jason@puglisi.im>
2017-04-28 23:26:24 -0400

    Added batch scripts for IRSec 2017

---
+++ competitions/irsec-2017/ftp-mysql-server.sh
@@ -0,0 +1,10 @@
+# Run scripts for FTP/MySQL server
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../scripts && pwd )"
+
+$DIR/clear-aliases.sh
+$DIR/clear-crontabs.sh
+$DIR/secure-sshd.sh
+$DIR/configure-ufw.sh 20/tcp,21/tcp,22/tcp,3306/tcp 9000/tcp
+$DIR/backup-files.sh
+
---
+++ competitions/irsec-2017/http-server.sh
@@ -0,0 +1,10 @@
+# Run scripts for HTTP server
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../scripts && pwd )"
+
+$DIR/clear-aliases.sh
+$DIR/clear-crontabs.sh
+$DIR/secure-sshd.sh
+$DIR/configure-ufw.sh 22/tcp,80/tcp 3306/tcp,9000/tcp
+$DIR/backup-files.sh
+
