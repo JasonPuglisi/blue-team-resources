1c82669a0ac78c9168e9de5e491184d57963e642
Jason Puglisi <jason@puglisi.im>
2017-04-29 09:25:29 -0400

    Add ssh outbound rules

--- competitions/irsec-2017/http-server.sh
+++ competitions/irsec-2017/http-server.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../scripts && pwd )"
 $DIR/clear-aliases.sh
 $DIR/clear-crontabs.sh
 $DIR/secure-sshd.sh
-$DIR/configure-ufw.sh 22/tcp,80/tcp 3306/tcp,9000/tcp
+$DIR/configure-ufw.sh 22/tcp,80/tcp 22/tcp,3306/tcp,9000/tcp
 $DIR/backup-files.sh
 
